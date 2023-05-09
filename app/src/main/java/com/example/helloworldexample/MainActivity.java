013ec2cd052966a64b2bbe9ee13837c2a17bbd5e
MarianBenner-Wickner <marian.benner-wickner@iu.org>
2023-05-09 11:17:58 +0200

    Image and Button added Added some functionality when button clicked

--- app/src/main/java/com/example/helloworldexample/MainActivity.java
+++ app/src/main/java/com/example/helloworldexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworldexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+        Button btn = findViewById(R.id.button);
+        btn.setText("Thank you!");
     }
 }
