8b8f1ab565084b05b89bb18a086ca91b3726f4c4
MarianBenner-Wickner <marian.benner-wickner@iu.org>
2023-05-23 18:40:09 +0200

    added lifecycle event observer

--- app/src/main/java/com/example/helloworldexample/MainActivity.java
+++ app/src/main/java/com/example/helloworldexample/MainActivity.java
@@ -1,21 +1,35 @@
 package com.example.helloworldexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LifecycleEventObserver {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getLifecycle().addObserver(this);
+
     }
 
     public void buttonClicked(View view) {
         Button btn = findViewById(R.id.button);
         btn.setText("Thank you!");
+    }
+
+    @Override
+    public void onStateChanged(@NonNull LifecycleOwner source,
+                               @NonNull Lifecycle.Event event) {
+        Log.i("LifecycleTest", "onStateChanged: " + event.name());
     }
 }
