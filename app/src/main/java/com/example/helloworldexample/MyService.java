01ee24667a74245d9ccae832b1db8cb1284cfc81
MarianBenner-Wickner <marian.benner-wickner@iu.org>
2023-05-31 17:31:12 +0200

    Service implementation running on the main thread (single-threaded example)

--- app/src/main/java/com/example/helloworldexample/MyService.java
+++ app/src/main/java/com/example/helloworldexample/MyService.java
@@ -2,47 +2,21 @@ package com.example.helloworldexample;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
-import android.os.Process;
 import android.widget.Toast;
 
 public class MyService extends Service {
-    private ServiceHandler serviceHandler;
-
-    private final class ServiceHandler extends Handler {
-        public ServiceHandler(Looper looper) {
-            super(looper);
-        }
-        @Override
-        public void handleMessage(Message msg) {
-            try {
-                Thread.sleep(4000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            stopSelf(msg.arg1);
-        }
-    }
-
-    @Override
-    public void onCreate() {
-        HandlerThread thread = new HandlerThread("ServiceStartArguments",
-                Process.THREAD_PRIORITY_BACKGROUND);
-        thread.start();
-        serviceHandler = new ServiceHandler(thread.getLooper());
-    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
 
-        Message msg = serviceHandler.obtainMessage();
-        msg.arg1 = startId;
-        serviceHandler.sendMessage(msg);
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        stopSelf();
 
         return START_STICKY;
     }
